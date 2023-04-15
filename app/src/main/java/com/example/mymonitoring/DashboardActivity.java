076e9b9de45027deec08f0aa07cae552dc79b274
Dedy Yusman <bangdhedot@gmail.com>
2023-04-15 13:25:21 +0700

    update ui

--- app/src/main/java/com/example/mymonitoring/DashboardActivity.java
+++ app/src/main/java/com/example/mymonitoring/DashboardActivity.java
@@ -4,11 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.denzcoskun.imageslider.ImageSlider;
+import com.denzcoskun.imageslider.constants.ScaleTypes;
+import com.denzcoskun.imageslider.models.SlideModel;
+
+import java.util.ArrayList;
+
 public class DashboardActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dasboard);
+
+        ImageSlider imageSlider = findViewById(R.id.image_slider);
+
+        ArrayList<SlideModel> imageList = new ArrayList<>();
+
+        imageList.add(new SlideModel(R.drawable.banner, null));
+        imageList.add(new SlideModel(R.drawable.banner, null));
+        imageSlider.setImageList(imageList, ScaleTypes.FIT);
+
     }
 }
