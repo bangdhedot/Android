5909c0d311406208c6c321839f3194ad98c6bacf
Dedy Yusman <bangdhedot@gmail.com>
2023-04-28 16:04:12 +0700

    UI

--- app/src/main/java/com/example/mymonitoring/FormUploadActivity.java
+++ app/src/main/java/com/example/mymonitoring/FormUploadActivity.java
@@ -2,16 +2,24 @@ package com.example.mymonitoring;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.ImageView;
 
+import java.util.Calendar;
+
 public class FormUploadActivity extends AppCompatActivity {
+    private EditText dateEdt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form_upload);
+
+        dateEdt = findViewById(R.id.editTextTanggal);
 
 
         ImageView BackButton = findViewById(R.id.IVBack);
@@ -20,6 +28,32 @@ public class FormUploadActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 finish();
+            }
+        });
+
+        dateEdt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Calendar c = Calendar.getInstance();
+
+                int year = c.get(Calendar.YEAR);
+                int month = c.get(Calendar.MONTH);
+                int day = c.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog datePickerDialog = new DatePickerDialog(
+
+                        FormUploadActivity.this,
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker view, int year,
+                                                  int monthOfYear, int dayOfMonth) {
+                                // on below line we are setting date to our edit text.
+                                dateEdt.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
+
+                            }
+                        },
+                        year, month, day);
+                datePickerDialog.show();
             }
         });
     }
