fadf0dabe5c0b667b77c5be67fd62ac3b6140a35
Dedy Yusman <bangdhedot@gmail.com>
2023-06-08 13:18:20 +0700

    updated

--- app/src/main/java/com/example/mymonitoring/ProfileActivity.java
+++ app/src/main/java/com/example/mymonitoring/ProfileActivity.java
@@ -6,13 +6,26 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.androidnetworking.AndroidNetworking;
+import com.androidnetworking.common.Priority;
+import com.androidnetworking.error.ANError;
+import com.androidnetworking.interfaces.JSONObjectRequestListener;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    TextView tvNama;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        tvNama = findViewById(R.id.TVNames);
 
         ImageView BackButton = findViewById(R.id.IVBack);
 
@@ -22,5 +35,29 @@ public class ProfileActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        getData();
+    }
+
+    private void getData() {
+        AndroidNetworking.get("https://dev.farizdotid.com/api/purwakarta/kuliner/1")
+                .setPriority(Priority.LOW)
+                .build()
+                .getAsJSONObject(new JSONObjectRequestListener() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        // do anything with response
+                        try {
+                            tvNama.setText(response.getString("nama"));
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                    @Override
+                    public void onError(ANError error) {
+                        // handle error
+                    }
+                });
     }
 }
